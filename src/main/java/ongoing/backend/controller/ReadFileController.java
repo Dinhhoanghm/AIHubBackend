1e2b5f2e8e1b7a6a165de32aba19c138eab153ee
Dinhhhoanghm <dodinhhoanghm@gmail.com>
2024-12-17 16:18:40 +0700

    add xml and json

--- src/main/java/ongoing/backend/controller/ReadFileController.java
+++ src/main/java/ongoing/backend/controller/ReadFileController.java
@@ -1,5 +1,6 @@
 package ongoing.backend.controller;
 
+import ongoing.backend.config.exception.ApiException;
 import ongoing.backend.service.ReadFileService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +21,8 @@ public class ReadFileController {
 
   @GetMapping("/download")
   public ResponseEntity<String> readFile(@RequestParam("url") String url,
-                                         @RequestParam("fileName") String fileName) throws IOException {
-    return ResponseEntity.ok(readFileService.convertFileToAvro(url, fileName));
+                                         @RequestParam("fileName") String fileName,
+                                         @RequestParam("fileType") String fileType) throws IOException, ApiException {
+    return ResponseEntity.ok(readFileService.convertFileToAvro(url, fileName, fileType));
   }
 }
--- src/main/java/ongoing/backend/service/ReadFileService.java
+++ src/main/java/ongoing/backend/service/ReadFileService.java
@@ -1,11 +1,13 @@
 package ongoing.backend.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
+import ongoing.backend.config.exception.ApiException;
+import ongoing.backend.config.jackson.json.JsonArray;
+import ongoing.backend.config.jackson.json.JsonObject;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.spark.SparkConf;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SaveMode;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.*;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.Metadata;
 import org.apache.spark.sql.types.StructField;
@@ -20,13 +22,18 @@ import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
-import java.util.UUID;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static ongoing.backend.config.jackson.json.Json.encode;
 
 @Service
 public class ReadFileService {
 
-  public String convertFileToAvro(String fileUrl, String fileName) throws IOException {
-    String filePath = downloadFile(fileUrl, fileName);
+  public String convertFileToAvro(String fileUrl, String fileName, String fileType) throws IOException, ApiException {
+    String filePath = downloadFile(fileUrl, fileName, fileType);
     return readFile(filePath);
   }
 
@@ -37,43 +44,104 @@ public class ReadFileService {
     SparkSession spark = SparkSession.builder()
       .config(conf)
       .getOrCreate();
-    Dataset<Row> firstRow = spark.read()
-      .option("header", "false")  // Don't use the first row as headers
-      .csv(filePath)
-      .limit(1);  // Only get the first row
+    Dataset<Row> df = null;
 
-    Row row = firstRow.first();
-    String[] columnNames = new String[row.size()];
-    for (int i = 0; i < row.size(); i++) {
-      columnNames[i] = row.getString(i); // Get each value as a string
+    if (filePath.endsWith(".csv")) {
+
+      Dataset<Row> firstRow = spark.read()
+        .option("header", "false")
+        .csv(filePath)
+        .limit(1);
+
+      Row row = firstRow.first();
+      String[] columnNames = new String[row.size()];
+      for (int i = 0; i < row.size(); i++) {
+        columnNames[i] = row.getString(i);
+      }
+
+      StructType schema = new StructType();
+      for (String columnName : columnNames) {
+        schema = schema.add(new StructField(columnName, DataTypes.StringType, true, Metadata.empty()));
+      }
+
+      df = spark.read()
+        .option("header", "true")
+        .schema(schema)
+        .csv(filePath);
+    }
+    if (filePath.endsWith(".xml")) {
+      df = spark.read()
+        .format("xml")
+        .option("rowTag", "row")
+        .option("attributePrefix", "_")
+        .load(filePath);
+
+
+      df = df.selectExpr("explode(row) as nestedRow")
+        .select("nestedRow.*");
+
+    }
+    if (filePath.endsWith(".json")) {
+      ObjectMapper objectMapper = new ObjectMapper();
+      String content = encode(objectMapper.readValue(new File(filePath), Object.class));
+      JsonObject data = new JsonObject(content);
+      JsonObject jsonObject = data
+        .getJsonObject("meta")
+        .getJsonObject("view");
+      List<Object> object = jsonObject.getJsonArray("columns")
+        .getList();
+      List<String> columnNames = object
+        .stream()
+        .map(s -> new JsonObject(encode(s)).getString("name"))
+        .collect(Collectors.toList());
+      StructType schema = new StructType();
+      for (String columnName : columnNames) {
+        schema = schema.add(new StructField(columnName, DataTypes.StringType, true, Metadata.empty()));
+      }
+      JsonArray value = data.getJsonArray("data");
+      List<Row> dataList = new ArrayList<>();
+      List<List<?>> dataObjects = value.getList();
+
+      for ( List<?> dataObject : dataObjects) {
+        dataObject = dataObject.stream().map(s -> encode(s)).collect(Collectors.toList());
+        dataList.add(RowFactory.create(dataObject.toArray()));
+      }
+      df = spark.createDataFrame(dataList, schema);
     }
 
-    StructType schema = new StructType();
-    for (String columnName : columnNames) {
-      schema = schema.add(new StructField(columnName, DataTypes.StringType, true, Metadata.empty()));
-    }
+    String outputFilePath = "/home/hoangdd/Downloads/output";
 
-    Dataset<Row> df = spark.read()
-      .option("header", "true")
-      .schema(schema)
-      .csv(filePath);
-    String currentDir = System.getProperty("user.dir");
-    String outputFilePath = currentDir + File.separator + filePath + "output" + UUID.randomUUID();
     df.write()
       .mode(SaveMode.Overwrite)
       .parquet(outputFilePath);
 
-    // Stop the Spark session
     spark.stop();
 
     return "Parquet file created successfully";
+
   }
 
 
-  public String downloadFile(String fileNameUrl, String fileName) throws IOException {
+  public String downloadFile(String fileNameUrl, String fileName, String fileType) throws IOException, ApiException {
     ReadableByteChannel readableByteChannel = Channels.newChannel(new URL(fileNameUrl).openStream());
     String currentDir = System.getProperty("user.dir");
-    String filePath = currentDir + File.separator  + File.separator + fileName + ".csv";
+    String fileEndPoint = null;
+    if (fileType.equalsIgnoreCase("csv")) {
+      fileEndPoint = ".csv";
+    }
+    if (fileType.equalsIgnoreCase("xml")) {
+      fileEndPoint = ".xml";
+    }
+    if (fileType.equalsIgnoreCase("json")) {
+      fileEndPoint = ".json";
+    }
+    if (fileType.equalsIgnoreCase("xls")) {
+      fileEndPoint = ".xls";
+    }
+    if (StringUtils.isBlank(fileType)) {
+      throw new ApiException("File Type Must Be Defined Before Download");
+    }
+    String filePath = currentDir + File.separator + fileName + fileEndPoint;
     try (FileOutputStream fileOutputStream = new FileOutputStream(filePath);
          FileChannel fileChannel = fileOutputStream.getChannel()) {
 
